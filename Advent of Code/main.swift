f91aa98813bccf436054c59e1881e4c7cdc1e412
Dimi Chakarov <dimitar@dchakarov.com>
2024-12-08 15:40:30 +0000

    part 1

--- Advent of Code/main.swift
+++ Advent of Code/main.swift
@@ -4,36 +4,63 @@
 //
 
 import Foundation
-import RegexHelper
 
 func main() {
     let fileUrl = URL(fileURLWithPath: "./aoc-input")
     guard let inputString = try? String(contentsOf: fileUrl, encoding: .utf8) else { fatalError("Invalid input") }
     
     let lines = inputString.components(separatedBy: "\n")
-        .filter { !$0.isEmpty }
     
-    // Sample algorithm
-    var scoreboard = [String: Int]()
+    var rules = [(Int, Int)]()
+    var updates = [[Int]]()
+    
+    var parsingRules = true
     lines.forEach { line in
-        let (name, score) = parseLine(line)
-        scoreboard[name] = score
+        if line.isEmpty {
+            parsingRules = false
+        } else {
+            if parsingRules {
+                let rule = parseRule(from: line)
+                rules.append(rule)
+            } else {
+                updates.append(parseUpdate(from: line))
+            }
+        }
     }
-    scoreboard
-        .sorted { lhs, rhs in
-            lhs.value > rhs.value
+
+    let correctUpdates = updates.filter { update in
+        for i in 0..<update.count {
+            let number = update[i]
+            let applicableRules = rules.filter { $0.0 == number }
+            if !applicableRules.isEmpty {
+                for rule in applicableRules {
+                    let number2 = rule.1
+                    if let updateIndex = update.firstIndex { $0 == number2 } {
+                        if i > updateIndex {
+                            return false
+                        }
+                    }
+                }
+            }
         }
-        .forEach { name, score in
-            print("\(name) \(score) pts")
-        }
+        return true
+    }
+    
+    let mids = correctUpdates.reduce(into: []) { partialResult, update in
+        partialResult.append(update[(update.count-1)/2])
+    }
+    
+    print(mids.reduce(0, +))
 }
 
-func parseLine(_ line: String) -> (name: String, score: Int) {
-    let helper = RegexHelper(pattern: #"([\-\w]*)\s(\d+)"#)
-    let result = helper.parse(line)
-    let name = result[0]
-    let score = Int(result[1])!
-    return (name: name, score: score)
+func parseRule(from line: String) -> (Int, Int) {
+    let comp = line.components(separatedBy: "|").map { Int($0)! }
+    return (comp[0], comp[1])
+}
+
+func parseUpdate(from line: String) -> [Int] {
+    let comp = line.components(separatedBy: ",")
+    return comp.map { Int($0)! }
 }
 
 main()
